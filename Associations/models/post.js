a6c8bf363df292eaeb4ebb01c6977119d8fd160d
EC2 Default User <ec2-user@ip-172-31-30-70.ap-southeast-1.compute.internal>
2020-11-26 20:24:02 +0000

    Simple concept of Associations and Refrences code with 'module.exports'

--- Associations/models/post.js
+++ Associations/models/post.js
@@ -5,3 +5,7 @@ var postSchema = new mongoose.Schema({
    content: String
 });
 module.exports = mongoose.model("Post", postSchema);
+
+// Module exports are the instruction that tells Node. js which 
+// bits of code (functions, objects, strings, etc.) to “export” from 
+// a given file so other files are allowed to access the exported code.
